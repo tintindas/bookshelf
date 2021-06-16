4d43cb1a4e89fe1e964b89e5df5a4cf9dbab25e4
Upamanyu Das <upamanyu.das.work@gmail.com>
2021-06-16 13:02:16 +0530

    add typescript

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var formatString = function (str) {
+    return str.split(' ').join('+');
+};
+console.log(formatString('tintin das'));
+// const addBooks = (title: string, author: string) : string => {
+// }
